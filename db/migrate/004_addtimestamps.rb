78c3858bdc7f6480d80101d919194000805d5fe2
kevin lang <langkevina@gmail.com>
2019-07-28 14:13:11 +0000

    Automatically backed up by Learn

--- db/migrate/004_addtimestamps.rb
+++ db/migrate/004_addtimestamps.rb
@@ -1,6 +1,6 @@
-class AddTimeStamps < ActiveRecord::Migration[5:1]
+class AddTimeStamps < ActiveRecord::Migration[5.1]
   def change
     add_column :students, :created_at, :datetime
     add_column :students, :created_on, :datetime
   end
-end  
+end
